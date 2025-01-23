cb560b2976d48bbca156343cc973855f9e94ae38
Marcoto06 <heroeoftime06@gmail.com>
2025-01-23 20:32:00 +0100

    Afegides escenes a la build, solucionat bug generació procedural, Botó per jugar al menú funcional

--- Tomato Game/Assets/Scripts/roomManager.cs
+++ Tomato Game/Assets/Scripts/roomManager.cs
@@ -8,7 +8,7 @@ public class roomManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        num_rooms = 1;
     }
 
     // Update is called once per frame
