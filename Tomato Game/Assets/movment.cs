d3046f5076291b7121f5868869ff6fd556dfccdc
guifre1714 <marcelpm2018@gmail.com>
2025-01-24 14:26:36 +0100

    salt perfecte i dash perfecte

--- Tomato Game/Assets/movment.cs
+++ Tomato Game/Assets/movment.cs
@@ -6,12 +6,13 @@ public class movment : MonoBehaviour
 {
     public float moveSpeed;
     [Header("Jump System")]
-    [SerializeField] float jumpTime;
-    [SerializeField] float jumpForce;
-    [SerializeField] float fallMultiplaier;
-    [SerializeField] float jumpMultiplaier;
+    [SerializeField] private float jumpTime;
+    [SerializeField] private float jumpForce;
+    [SerializeField] private float fallMultiplaier;
+    [SerializeField] private float jumpMultiplaier;
+    [SerializeField] private TrailRenderer tr;
 
-    Rigidbody2D rb;
+    private Rigidbody2D rb;
 
     public Transform groundCheck;
 
@@ -19,14 +20,21 @@ public class movment : MonoBehaviour
 
     Vector2 vecGravity;
 
-    bool isJumping;
-    float jumpCounter;
+    private bool isFacingRight = true;
+
+    private bool isJumping;
+    private float jumpCounter;
+
+    private float horizontal;
+    private bool canDash = true;
+    private bool isDashing;
+    public float dashForce;
+    public float dashingTime;
+    public float dashingCooldown;
 
     public Animator anim;
     public SpriteRenderer PlayerSR;
 
-    
-    // Start is called before the first frame update
     void Start()
     {
         vecGravity = new Vector2 (0, -Physics2D.gravity.y);
@@ -34,10 +42,16 @@ public class movment : MonoBehaviour
 
     }
 
-    // Update is called once per frame
     void Update()
     {
-        rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, rb.velocity.y);
+        if (isDashing)
+        {
+            return;
+        }
+
+        horizontal = Input.GetAxisRaw("Horizontal");
+
+        Flip();
 
         if (Input.GetButtonDown("Jump") && isGrounded())
         {
@@ -50,10 +64,14 @@ public class movment : MonoBehaviour
         {
             jumpCounter = Time.deltaTime;
             if (jumpCounter > jumpTime) isJumping = false;
-                     rb.velocity += vecGravity * jumpMultiplaier * Time.deltaTime;
+            rb.velocity += vecGravity * jumpMultiplaier * Time.deltaTime;
         }
 
         if (Input.GetButtonUp("Jump"))
+        {
+            isJumping = false;
+        }
+        if (Input.GetButtonDown("Jump"))
         {
             isJumping = false;
         }
@@ -62,10 +80,53 @@ public class movment : MonoBehaviour
         {
             rb.velocity -= vecGravity * fallMultiplaier * Time.deltaTime;
         }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift) && canDash)
+        {
+            StartCoroutine(Dash());
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (isDashing)
+        {
+            return;
+        }
+
+        rb.velocity = new Vector2(horizontal * moveSpeed, rb.velocity.y);
+    }
+
+    private void Flip()
+    {
+        if (isFacingRight && horizontal<0f || !isFacingRight && horizontal>0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
     }
 
     bool isGrounded()
     {
         return Physics2D.OverlapCapsule(groundCheck.position, new Vector2(1.09f, 0.2f), CapsuleDirection2D.Horizontal, 0, whatIsGround);
+    }
+
+    private IEnumerator Dash()
+    {
+        canDash = false;
+        isDashing = true;
+        float originalGravity = rb.gravityScale;
+        rb.gravityScale = 0f;
+        rb.velocity = new Vector2(transform.localScale.x * dashForce, 0f);
+        tr.emitting = true;
+        yield return new WaitForSeconds(dashingTime);
+        tr.emitting = false;
+        rb.gravityScale = originalGravity;
+        isDashing = false;
+        yield return new WaitForSeconds(dashingCooldown);
+        canDash = true;
+
     }
 }
