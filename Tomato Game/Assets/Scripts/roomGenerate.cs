c9baa991725cba481e61838e4b72db89b5859077
Marcoto06 <heroeoftime06@gmail.com>
2025-01-24 13:48:20 +0100

    menu acabat d'implementar. millorat generacio procedural

--- Tomato Game/Assets/Scripts/roomGenerate.cs
+++ Tomato Game/Assets/Scripts/roomGenerate.cs
@@ -5,17 +5,50 @@ using UnityEngine;
 public class roomGenerate : MonoBehaviour
 {
     public GameObject manager;
-    public GameObject[] L1Rooms;
+    public GameObject[] Rooms;
+    public GameObject finalRoom;
+    public GameObject safeRoom;
+    public GameObject upRoom;
+    public int floor;
     private int rand;
     // Start is called before the first frame update
     void Start()
     {
-        rand = Random.Range(0, L1Rooms.Length);
-        if (manager.GetComponent<roomManager>().num_rooms < 5)
-        {
-            Instantiate(L1Rooms[rand], transform.position, L1Rooms[rand].transform.rotation);
+        rand = Random.Range(0, Rooms.Length);
+        if (manager.GetComponent<roomManager>().num_rooms < 10)
+        {   
+            if (Rooms[rand].tag == "upRoom")
+            {
+                if (manager.GetComponent<roomManager>().created_upRoom == false)
+                {
+                    Instantiate(Rooms[rand], transform.position, Rooms[rand].transform.rotation);
+                    manager.GetComponent<roomManager>().created_upRoom = true;
+                } else { Instantiate(Rooms[0], transform.position, Rooms[0].transform.rotation); }
+            }
+            else if (Rooms[rand].tag == "safeRoom")
+            {
+                if (manager.GetComponent<roomManager>().created_safeRoom == false)
+                {
+                    Instantiate(Rooms[rand], transform.position, Rooms[rand].transform.rotation);
+                    manager.GetComponent<roomManager>().created_safeRoom = true;
+                } else { Instantiate(Rooms[0], transform.position, Rooms[0].transform.rotation); }
+            }
+            else {
+                Instantiate(Rooms[rand], transform.position, Rooms[rand].transform.rotation); 
+            }
             manager.GetComponent<roomManager>().num_rooms++;
-        } else { manager.GetComponent<roomManager>().num_rooms = 1; }
+        }
+        else if (manager.GetComponent<roomManager>().created_safeRoom == false)
+        {
+            Instantiate(safeRoom, transform.position, safeRoom.transform.rotation);
+            manager.GetComponent<roomManager>().created_safeRoom = true;
+        }
+        else if (manager.GetComponent<roomManager>().created_upRoom == false)
+        {
+            Instantiate(upRoom, transform.position, upRoom.transform.rotation);
+            manager.GetComponent<roomManager>().created_upRoom = true;
+        }
+        else { Instantiate(finalRoom, transform.position, finalRoom.transform.rotation); manager.GetComponent<roomManager>().num_rooms = 1; manager.GetComponent<roomManager>().created_upRoom = false; manager.GetComponent<roomManager>().created_safeRoom = false; }
     }
 
     // Update is called once per frame
