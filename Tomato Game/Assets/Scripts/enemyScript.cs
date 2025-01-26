39efe196fbc0fea166744ee52cf92e97529462e6
Marcoto06 <heroeoftime06@gmail.com>
2025-01-26 17:46:48 +0100

    creat template de enemics

---
+++ Tomato Game/Assets/Scripts/enemyScript.cs
@@ -0,0 +1,34 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyScript : MonoBehaviour
+{
+    public string EN_type;
+    public int HP;
+    public float moveSpeed;
+    public bool attacking;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(EN_type == "figa")
+        {
+            
+        }
+        else if (EN_type == "pinya")
+        {
+             
+        }
+        else if (EN_type == "llimona")
+        {
+
+        }
+    }
+}
